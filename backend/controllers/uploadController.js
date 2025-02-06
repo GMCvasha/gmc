dd7d1c7f4d60f1e5be9bf801e3109aa46a055f21
TK <tk@TEE.TEE>
2025-02-06 11:32:11 +0300

    Church commit

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -59,8 +59,16 @@ const updateUserImage = async (req, res) => {
   }
 };
 
-module.exports = { updateUserImage };
+const getAllImages = async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.status(200).json(images);
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    res.status(500).json({ message: 'Internal server error.' });
+  }
+};
 
-
+module.exports = { updateUserImage, getAllImages };
 
 
--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { updateUserImage } = require("../controllers/uploadController");
+const { updateUserImage, getAllImages } = require("../controllers/uploadController");
 const upload = require('../middleware/upload');
 
 const router = express.Router();
 
 // Route for uploading a file
 router.put('/image', upload.single('image'), updateUserImage);
-
+router.get('/images', getAllImages);
 module.exports = router;
 
